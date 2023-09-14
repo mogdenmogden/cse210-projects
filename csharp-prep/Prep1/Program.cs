cdfda9389e369e17478f40087a5927a651bef36a
Matt Ogden <ogd21005@byui.edu>
2023-09-13 19:08:16 -0500

    second try, line change

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        #Console.WriteLine("Hello Prep1 World!");
-        Console.WriteLine("What is your first name? ");
+        Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
-        Console.WriteLine("What is your last name? ");
+        Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
         Console.WriteLine();
         Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
