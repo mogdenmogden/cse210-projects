ce52a4d083c4982c2ae4b09b6a4fbe1f4716df46
Matt Ogden <ogd21005@byui.edu>
2023-09-20 12:37:06 -0500

    Core reqmts

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int stopper = 1;
+        int theEntry;
+        int sumList;
+        double avgList;
+        int maxList;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+            {
+                Console.Write("Enter a number: ");
+                theEntry = int.Parse(Console.ReadLine());
+                if (theEntry == 0)
+                    {
+                        stopper = 0;
+                    }
+                else
+                    numbers.Add(theEntry);
+
+            } while (stopper != 0);
+        
+        sumList = numbers.Sum();
+        Console.WriteLine($"The sum is: {sumList}");
+        avgList = numbers.Average();
+        Console.WriteLine($"The average is: {avgList}");
+        maxList = numbers.Max();
+        Console.WriteLine($"The largest is: {maxList}");
+
     }
 }
