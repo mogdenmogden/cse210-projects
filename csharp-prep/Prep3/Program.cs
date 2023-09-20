75cc710cf862c41fa50a50c5ed7f14c877465eb2
Matt Ogden <ogd21005@byui.edu>
2023-09-20 07:56:40 -0500

    Step 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string numberInput = Console.ReadLine();
+        int randomNumber = int.Parse(numberInput);
+        
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+        int theGuess = int.Parse(guessInput);
+
+        if (theGuess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        else if (theGuess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        else
+        Console.WriteLine("You guessed it!");
+        
+        Console.WriteLine(randomNumber);
     }
 }
