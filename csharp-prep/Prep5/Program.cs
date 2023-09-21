2822f6b4d45da0634e1ed11dcf21ad69858b42a9
Matt Ogden <ogd21005@byui.edu>
2023-09-20 19:22:49 -0500

    Step 3

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string username = Console.ReadLine();
+            return username;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int faveNumber = int.Parse(Console.ReadLine());
+            return faveNumber;
+        }
+
+        static int SquareNumber(int nbr)
+        {
+            int squared = nbr*nbr;
+            return squared;
+        }
+
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"{name}, the square of your number is {number}.");
+        }
+
+        DisplayWelcome();
+        string usrName = PromptUserName();
+        int usrNumber = PromptUserNumber();
+        int sqrNumber = SquareNumber(usrNumber);
+        DisplayResult(usrName,sqrNumber);
+
     }
 }
