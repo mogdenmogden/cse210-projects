35596b2206888082917f8bd0b66bdb3ea9f203f4
Matt Ogden <ogd21005@byui.edu>
2023-12-09 13:04:56 -0600

    12/9/2023 1:05 pm a bit cleaner

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -2,24 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 public abstract class Activity
 {
-    // protected string _date;
-    // protected double _duration;
     protected double _distance;
-
-    // public Activity(string date, double duration, double distance)
-    // {
-    //    _date = date;
-    //     _duration = duration;
-    //     _distance = distance;
-    // }
-
     public abstract void GetSummary();
-    // public virtual void GetSummary()
-    // {
-    //     Console.WriteLine($"-- {_date} {GetType()} ({_duration} min) - Distance {_distance.ToString("N2")} km, Speed: {GetSpeed(_duration,_distance).ToString("N2")} kph, Pace: {GetPace(_duration,_distance).ToString("N2")} min per km");
-    // }
-
-
+    
     protected virtual double GetSpeed(double duration,double distance)
     {
         return 60*(distance/duration); //kph
@@ -31,13 +16,8 @@ public abstract class Activity
     }
 
     public abstract string GetDate();
-    // {
-    // }
-
+    
     public abstract double GetDuration();
-    // {
-    //     return _duration;
-    // }
 
     public virtual double GetDistance()
     {
